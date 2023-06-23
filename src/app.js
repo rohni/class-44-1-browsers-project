c6382b7787348a30b4f7ca90e6ca78808fb294fb
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2023-06-23 10:22:03 +0200

    res

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 //import { quizData } from './data.js';
 //import { initWelcomePage } from './pages/welcomePage.js';
 
-const loadApp = () => {
-  quizData.currentQuestionIndex = 0;
+// const loadApp = () => {
+//   quizData.currentQuestionIndex = 0;
 
-  initWelcomePage();
-};
+//   initWelcomePage();
+// };
 
-window.addEventListener('load', loadApp);
+// window.addEventListener('load', loadApp);
 
 
 //console.log('loadApp');
