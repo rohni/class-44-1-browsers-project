e74028ac5057ae751c262d8c9a4946e551a3c71a
Rohan Nicholls <rohan.nicholls@gmail.com>
2023-06-24 10:28:34 +0200

    Timer working

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,21 +1,3 @@
-// <<<<<<< timer-attempt
-// import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
-// import { startTimer } from '../views/timerView.js';
-// import { createWelcomeElement } from '../views/welcomeView.js';
-// import { initQuestionPage } from './questionPage.js';
-// =======
-// //import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
-// //import { createWelcomeElement } from '../views/welcomeView.js';
-// //import { initQuestionPage } from './questionPage.js';
-// >>>>>>> main
-
-// export const initWelcomePage = () => {
-//   const userInterface = document.getElementById(USER_INTERFACE_ID);
-//   userInterface.innerHTML = '';
-
-//   const welcomeElement = createWelcomeElement();
-//   userInterface.appendChild(welcomeElement);
-// welcomePage.js
 
 import { initQuestionPage } from './questionPage.js';
 
@@ -36,49 +18,7 @@ export const initWelcomePage = (userInterface) => {
   const startQuizButton = document.getElementById('start-quiz-button');
   startQuizButton.addEventListener('click', () => {
     initQuestionPage(userInterface);
+
   });
 };
 
-
-
-
-
-
-
-
-
-
-
-
-//   document
-//     .getElementById(START_QUIZ_BUTTON_ID)
-//     .addEventListener('click', startQuiz);
-
-// <<<<<<< timer-attempt
-// const startQuiz = () => {
-//   initQuestionPage();
-//   startTimer();
-// };
-// =======
-// // };
-// >>>>>>> main
-
-// const startQuiz = () => {
-//   initQuestionPage();
-// };
-
-// Update from diaa
-
-// const initWelcomePage = () => {
-//   const userInterface = document.getElementById(USER_INTERFACE_ID);
-//   userInterface.innerHTML = '';
-
-//   const welcomeElement = createWelcomeElement();
-//   userInterface.appendChild(welcomeElement);
-
-//   document.getElementById(START_QUIZ_BUTTON_ID).addEventListener('click', startQuiz);
-// };
-
-// const startQuiz = () => {
-//   initQuestionPage();
-// };
--- src/views/timer.js
+++ src/views/timer.js
@@ -1,41 +1,56 @@
-import { nextQuestion } from './pages/questionPage.js';
-
+const timerEl = document.getElementById('timeLeft');
+const allowedTime = 30;
 let interval;
+let sec = allowedTime;
+
 //export in order we need it for other thing
 //inside the timer function we need an element as it is in the HTML with id attribute timeLeft
-export function timer() {
-  let timerEl = document.getElementById('timeLeft');
-  let sec = 30;
-//created countdown function which is decreases the sec variable above by 1
-  function countdown() {
-      sec--;
-      //so here each time we update the innerHTML of the timerEl decrease the sec 
-    timerEl.innerHTML = `00: ${sec}`;
-    if (sec < 10) {
-      timerEl.innerHTML = `00: 0${sec}`;
-    }
+export function stopTimer () {
+  clearInterval(interval);
+}
 
-    // when the time's up directly move on to the next question
+function nextQuestion () {
+  setTimeout(() => document.getElementById('next-button').click(), 1000); 
+}
 
-    if (sec <= 0) {
-      timerEl.innerHTML = "Time's up!";
-      clearInterval(interval);
-      setTimeout(nextQuestion, 1000);
-    }
-  }
-  // start the timer and if the user pass to the next question clear the previous one
-  function startTimer() {
-    clearInterval(interval);
-    sec = 30;
-    interval = setInterval(countdown, 1000);
-  }
+export function startTimer() {
+  
+  clearInterval(interval);
+  sec = allowedTime;
+  setTimeDisplay(sec);
+  interval = setInterval(countdown, 2000);
+}
 
-  if (interval === undefined) {
-    startTimer();
-  } else {
-    timerEl.innerHTML = '';
-    startTimer();
+function setTimeDisplay(sec) {
+  timerEl.innerHTML = `00: ${sec}`;
+  if (sec < 10) {
+    timerEl.innerHTML = `00: 0${sec}`;
   }
 }
 
+//created countdown function which is decreases the sec variable above by 1
+function countdown() {
+  sec--;
+    //so here each time we update the innerHTML of the timerEl decrease the sec 
+  setTimeDisplay(sec);
+  // when the time's up directly move on to the next question
+  if(sec <= 0){
+    timerEl.innerHTML = "Time's up!";
+    stopTimer()
+    nextQuestion()
+  }
+}
 
+export function timer() {
+  // if we are on a question page, then start timer.
+  const answers = document.getElementById('options');
+  if (answers && answers.children.length > 0) {
+    startTimer();
+    answers.addEventListener('change', () => {
+      stopTimer();
+      nextQuestion();
+    });
+  } else {
+    timerEl.innerHTML = 'Countdown';
+  }
+}
